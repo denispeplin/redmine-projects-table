3f621f88e0b3d4d4ebb49f236dc84985a0324dd0
Denis Peplin <denis.peplin@gmail.com>
2013-01-07 21:00:31 +0400

    Output standart projects page using self-made method.

--- lib/projects_table.rb
+++ lib/projects_table.rb
@@ -1,9 +1,16 @@
 module ProjectsTable
   def self.included(base)
     base.send(:include, InstanceMethods)
+    base.class_eval do
+      alias_method_chain :render_project_hierarchy, :projects_table
+    end
   end
 
   module InstanceMethods
-    
+    def render_project_hierarchy_with_projects_table(projects)
+      render_project_nested_lists(projects) do |project|
+        link_to_project(project, {}, :class => "#{project.css_classes} #{User.current.member_of?(project) ? 'my-project' : nil}")
+      end
+    end
   end
 end
