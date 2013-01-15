e2ee0cabd16b3cdaa4723c153d66e5a8fe609735
denispeplin <denis.peplin@gmail.com>
2013-01-15 13:23:35 +0400

    Update version to 0.0.4 (Ruby 1.8 support)

--- init.rb
+++ init.rb
@@ -2,7 +2,7 @@ Redmine::Plugin.register :projects_table do
   name 'Projects Table plugin'
   author 'Denis Peplin'
   description 'This plugin adds table view to Projects page'
-  version '0.0.3'
+  version '0.0.4'
   url 'https://github.com/denispeplin/redmine-projects-table'
   author_url 'https://github.com/denispeplin'
 end
